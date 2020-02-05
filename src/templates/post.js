43f1bbd8e47843c3489a5ba19f6fdcf0301340e4
Christina <christinavhastenrath@gmail.com>
2020-02-05 10:36:33 -0800

    inline-images are now showing

--- src/templates/post.js
+++ src/templates/post.js
@@ -48,53 +48,21 @@ const BlogPostTemplate = ({ data }) => {
   const date = data.wpgraphql.post.date
   const featuredImage = data.wpgraphql.post.featuredImage;
 
-  // plugin is parsing the post content to grab the URL
-  // const pluginOptions = {
-  //   wordPressUrl: `https://blog.postman.com`,
-  //   uploadsUrl: `https://blog.postman.com/wp-content/uploads/`,
-  // };
-  
     return (
       <Layout>
         <SEO title="post"/>
         <FluidImage image={featuredImage} />
         <h1 dangerouslySetInnerHTML={{ __html: title }} />
         <EntryMeta name={name} avatar={avatar} date={date}/>
-        {/* <div>{contentParser({ content },  pluginOptions )}</div> */}
         {/* <div dangerouslySetInnerHTML={{ __html: content }} /> */}
-        {/* <p>{ReactHtmlParser(content)}</p> */}
 
         <div>{parse(content, {
           replace: (domNode) => {
 
-           let imageUrl = domNode.name === 'img' && domNode.attribs["data-src"];
-           let name = document.getElementsByTagName("img");
-    
-           // forEach / map refine . filter only for images that have a data-src
-        
-       
-          
-            if (imageUrl) {
-              console.log("//////////I grabbed the image BOOOMMMM  //////////////////", name)
-              console.log('type of name:', typeof(name))
-              // console.log('dataset:', name[dataset])
-              
-              // for (let i in name) {
-              //   // if (name[i].getAttribute("img")) {
-              //   //   console.log('got the image')
-              //   // }
-              //   console.log('in for:', name[i])
-              //   // name[i].setAttribute("src", name[i].getAttribute("data-src"));
-              // }
-
-              //  name[i].setAttribute("src", name[i].getAttribute("data-src"));
-            }
- 
-          
-          
-          
-          
-          // name.setAttribute("src", name.getAttribute("data-src")); 
+          if (domNode.attribs && domNode.attribs['data-src']) {
+            console.log(domNode);
+            return <img src={domNode.attribs['data-src']} alt={domNode.attribs.alt} />
+           }
           }
         })}</div>
    
