786662d3c903fcd6c100069034a9a7447ee13764
Christina <christinavhastenrath@gmail.com>
2020-02-26 10:11:54 -0800

    wip algolia debouncing

--- src/components/Search/searchPreview.jsx
+++ src/components/Search/searchPreview.jsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connectSearchBox, connectHits } from 'react-instantsearch-dom';
 
 import './_search.scss';
 
+class SearchBox extends Component {
+  Counter = 3; 
 
-const SearchBox = ({ currentRefinement, refine }) => (
-  <div className="ais-SearchBox">
-    <form noValidate action="" role="search" className="ais-SearchBox-form">
-      <input
-        className="ais-SearchBox-input"
-        type="search"
-        value={currentRefinement}
-        onChange={(event) => refine(event.currentTarget.value)}
-      />
-    </form>
-  </div>
-);
+  state = {
+    value: this.props.currentRefinement
+  };
+
+  onKeyStroke = event => {
+    const { refine } = this.props;
+    const value = event.currentTarget.value;
+    console.log('refine/////////////////', refine)
+
+    clearTimeout(this.Counter);
+    this.Counter = setTimeout(() => refine(value))
+
+    this.setState(() => ({
+      value
+    }));
+  }
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div className="ais-SearchBox">
+      <form noValidate action="" role="search" className="ais-SearchBox-form">
+      <input 
+        value={value}
+        onChange={this.onKeyStroke}
+        placeholder="Search for products with 3 keystrokes...."
+        />
+          </form>
+      </div>
+    )
+  }
+}
+
+// const SearchBox = ({ currentRefinement, refine }) => (
+//   <div className="ais-SearchBox">
+//     <form noValidate action="" role="search" className="ais-SearchBox-form">
+//       <input
+//         className="ais-SearchBox-input"
+//         type="search"
+//         value={currentRefinement}
+//         onChange={(event) => refine(event.currentTarget.value)}
+//       />
+//     </form>
+//   </div>
+// );
 
 export const CustomSearchBox = connectSearchBox(SearchBox);
 
