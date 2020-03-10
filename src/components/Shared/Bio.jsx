f4b36253d00bfdaff7fd31e48c2c92066350bd0e
Christina <christinavhastenrath@gmail.com>
2020-03-10 16:34:48 -0700

    added bio

---
+++ src/components/Shared/Bio.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Bio = ({ authorBio, avatar, name, authorSlug }) => (
+  <div className="row bio">
+    <div className="col-2">
+      <img className="entry-meta-img" src={avatar} alt={name} />
+    </div>
+    <div className="col-10">
+      <div className="row">
+        <div className="col-12">
+          {name} 
+        </div>
+        <div className="col-12 v5-link">
+          {authorBio}
+          <Link to={`/${authorSlug}/page/1`}> See more posts of {name}</Link>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Bio;
