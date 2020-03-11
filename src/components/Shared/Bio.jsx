4d2ae00299d7b32577523b0e4b0ab82c45d334c3
Christina <christinavhastenrath@gmail.com>
2020-03-10 17:21:25 -0700

    added bio

--- src/components/Shared/Bio.jsx
+++ src/components/Shared/Bio.jsx
@@ -3,10 +3,10 @@ import { Link } from 'gatsby';
 
 const Bio = ({ authorBio, avatar, name, authorSlug }) => (
   <div className="row bio">
-    <div className="col-2">
+    <div className="col-1">
       <img className="entry-meta-img" src={avatar} alt={name} />
     </div>
-    <div className="col-10">
+    <div className="col-11">
       <div className="row">
         <div className="col-12">
           {name} 
