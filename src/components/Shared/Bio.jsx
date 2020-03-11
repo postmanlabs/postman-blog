665e54341e2122bc74d0dbe741a78b389a83d43e
Christina <christinavhastenrath@gmail.com>
2020-03-11 11:35:36 -0700

    working on author bio

--- src/components/Shared/Bio.jsx
+++ src/components/Shared/Bio.jsx
@@ -3,17 +3,19 @@ import { Link } from 'gatsby';
 
 const Bio = ({ authorBio, avatar, name, authorSlug }) => (
   <div className="row bio">
-    <div className="col-1">
-      <img className="entry-meta-img" src={avatar} alt={name} />
+    <div className="col-2">
+      <img className="bio-img" src={avatar} alt={name} />
     </div>
-    <div className="col-11">
+    <div className="col-10">
       <div className="row">
         <div className="col-12">
           {name} 
         </div>
         <div className="col-12 v5-link">
-          {authorBio}
-          <Link to={`/${authorSlug}/page/1`}> See more posts of {name}</Link>
+          <p>
+            {authorBio}
+            <Link to={`/${authorSlug}/page/1`}> See more posts of {name}</Link>
+          </p>
         </div>
       </div>
     </div>
