47cdee9af3261781dfc6778cb9ceebbd65c9c5a6
Christina <christinavhastenrath@gmail.com>
2020-03-18 09:46:54 -0700

    fix linting errors

--- src/components/FluidImage.jsx
+++ src/components/FluidImage.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import cooper from '../assets/postman-cooper-fallback.jpg';
 
-
-
-const FluidImage = ({ image, withFallback = false, ...props }) => {
+const FluidImage = ({ image, ...props }) => {
   // Return fallback Image, if no Image is given.
   if (!image) {
     return (
-      <img 
-        loading='lazy'
-        src={cooper} 
-        alt="Postman Cooper Default Image" 
+      <img
+        loading="lazy"
+        src={cooper}
+        alt="Postman Cooper Fallback"
         {...props}
       />
-    )
+    );
   }
 
   if (image && image.imageFile) {
     return (
       <img
-        loading='lazy'
+        loading="lazy"
         fluid={image.imageFile.childImageSharp.fluid}
         alt={image.altText}
         {...props}
