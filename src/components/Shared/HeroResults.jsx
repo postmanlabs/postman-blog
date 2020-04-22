7e39e6c895a450611e9e84019c4f4de4bfe4b97e
tristandenyer <tristan@tristandenyer.com>
2020-04-22 16:04:11 -0700

    fixed linting errors

--- src/components/Shared/HeroResults.jsx
+++ src/components/Shared/HeroResults.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
 const HeroResults = ({
-  title, totalPosts,
-}) => {
-  return (
-    <div className="row hero-results__container">
-      <div className="col-12">
-        <h1 className="h2 mb-0">{`Postman ${title[0].toUpperCase()}${title.slice(1)}`}</h1>
-      </div>
+  title,
+}) => (
+  <div className="row hero-results__container">
+    <div className="col-12">
+      <h1 className="h2 mb-0">{`Postman ${title[0].toUpperCase()}${title.slice(1)}`}</h1>
     </div>
-  );
-};
+  </div>
+);
 
 export default HeroResults;
