a72cb9e8da2b95d1f6c23935ff304b64c6119aec
Christina <christinavhastenrath@gmail.com>
2020-04-28 14:44:49 -0700

    fixed linting errors

--- src/components/Shared/CommentList.jsx
+++ src/components/Shared/CommentList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReturnDateString from './ReturnDateString';
-import Pingback from './Pingback';
+
 
 const Comment = ({ comments }) => (
   <div>
@@ -30,15 +30,14 @@ const Comment = ({ comments }) => (
           <div className="comments__approved">
             <div className="col-12">
               {
-                commentAuthor && commentAuthorUrl 
-                ? 
-                // <Pingback commentAuthorUrl={commentAuthorUrl} commentAuthor={commentAuthor} />
-                  <div>
-                  <h2>Pingback</h2>
-                  <a href={commentAuthorUrl} className="comment author with url">{commentAuthor}</a>
-                  </div> 
-                : 
-                  <div className="comment author without url">{commentAuthor}</div>
+                commentAuthor && commentAuthorUrl
+                  ? (
+                    <div>
+                      <h2>Pingback</h2>
+                      <a href={commentAuthorUrl} className="comment author with url">{commentAuthorUrl}</a>
+                    </div>
+                  )
+                  : <div className="comment author without url">{commentAuthor}</div>
               }
             </div>
             <div className="col-12">
