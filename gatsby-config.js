5aef4125ebba249914be518bb5bb90f703da161f
Christina <christinavhastenrath@gmail.com>
2020-05-01 14:12:25 -0700

    add tacking of plugin to head instead of body

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,7 +122,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-43979731-4",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
