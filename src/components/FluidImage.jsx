dc308577750cc789c33c9abe0efed6c7f3c53bcd
Christina <christinavhastenrath@gmail.com>
2020-05-12 14:56:30 -0700

    added props back in to fix styling

--- src/components/FluidImage.jsx
+++ src/components/FluidImage.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 // import cooper from '../assets/postman-cooper-fallback.jpg';
 
-const FluidImage = ({ image }) => {
+const FluidImage = ({ image, ...props }) => {
   // Return fallback Image, if no Image is given.
   // const editImage = image.sourceUrl.replace("blog.postman.com", "edit.blog.postman.com")
   if (!image) {
@@ -30,7 +30,7 @@ const FluidImage = ({ image }) => {
   //   );
   // }
 
-  return <img src={image.sourceUrl} className="img-fluid" alt={image.altText} />;
+  return <img src={image.sourceUrl} className="img-fluid" alt={image.altText} {...props}/>;
 };
 
 export default FluidImage;
