29c998426dcbd4e9b9413a279a240977f0ff50a1
tristandenyer <tristan@tristandenyer.com>
2020-05-20 17:20:30 -0700

    updated app metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,7 +107,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
+        name: 'postman-blog-on-gatsby',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
