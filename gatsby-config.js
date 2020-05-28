c2e93f667e2c5dbd56455be6973bf88ef1d700dc
Christina <christinavhastenrath@gmail.com>
2020-05-28 15:55:15 -0700

    added csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,22 @@ module.exports = {
     'gatsby-plugin-meta-redirect',
     'gatsby-plugin-sitemap',
     {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeStyleHashes: true, // you can disable styles sha256 hashes
+        mergeDefaultDirectives: true,
+        directives: {
+          "script-src": "'self' www.google-analytics.com",
+          "style-src": "'self' 'unsafe-inline'",
+          "img-src": "'self' data: www.google-analytics.com"
+          // you can add your directives or override defaults
+        }
+      }
+    },
+    {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
         host: '',
