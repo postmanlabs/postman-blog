acaf2b520b011b059ad8efd7a0af91ec75c91024
Ryan-McBride <mcbrideryan89@gmail.com>
2020-06-19 11:07:54 -0700

    checking if exists before if includes

--- src/components/Shared/ReturnDateString.jsx
+++ src/components/Shared/ReturnDateString.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ReturnDateString = ({ data }) => {
   let dated;
-  if (!data.includes('T')) {
+  if (data && !data.includes('T')) {
     const arr = data.split(/[- :]/);
     dated = new Date(arr[0], arr[1] - 1, arr[2], arr[3], arr[4], arr[5]);
   } else {
