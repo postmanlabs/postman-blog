caa8ee20ef9bbcf4a558547390d458213bdc0a36
Christina <christinavhastenrath@gmail.com>
2020-08-03 11:21:38 -0700

    moved uuidv4 import around

--- src/components/Shared/EntryMeta.jsx
+++ src/components/Shared/EntryMeta.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import uuidv4 from 'uuid/v4';
 import ReturnDateString from './ReturnDateString';
+
 
 const EntryMeta = ({
   name, avatar, authorSlug, date,
 }) => (
-  <div className="entry-meta" key={Math.random()}>
+  <div className="entry-meta" key={uuidv4()}>
     <div className="entry-meta-col">
       <span><img className="entry-meta-img" src={avatar} alt={name} /></span>
       <span><Link to={`/author/${authorSlug}/page/1`}>{name}</Link></span>
